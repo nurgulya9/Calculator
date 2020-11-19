c0074a20310deb23b5e04cb56df1b3c2e95bad88
unknown <nm717171@mail.ru>
2020-11-19 15:24:33 -0600

    added metthod

--- src/calculator.java
+++ src/calculator.java
@@ -2,4 +2,6 @@ public class calculator {
     public static double add(double num1,double num2){
         return num1 + num2;
     }
+    public static double substract(double num1,double num2){
+        return num1-num2;    }
 }
