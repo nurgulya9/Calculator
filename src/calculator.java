7a52f7072468fd858a7306d1c1a8cd69b3878454
unknown <nm717171@mail.ru>
2020-11-19 15:45:14 -0600

    delete

--- src/calculator.java
+++ src/calculator.java
@@ -2,6 +2,5 @@ public class calculator {
     public static double add(double num1,double num2){
         return num1 + num2;
     }
-    public static double substract(double num1,double num2){
-        return num1-num2;    }
+
 }
