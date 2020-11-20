371e156728295c7b10513f8e66b0806790773119
unknown <nm717171@mail.ru>
2020-11-20 11:40:00 -0600

    dividing

--- src/calculator.java
+++ src/calculator.java
@@ -4,4 +4,6 @@ public class calculator {
     }
 public static double multiply(double num1,double num2){
     return num1*num2;
+    public static int divide(int num3,int num4 )
+    return num3/num4;
 }
